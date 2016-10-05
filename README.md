fc87fd407eb2fe13beb7f216df595b6013a2be82
Adsads <salzaustin@gmail.com>
2016-10-05 13:01:09 -0500

    Initial commit of Hacker-Terminal

---
+++ README.md
@@ -0,0 +1,8 @@
+## Synopsis
+
+Hacker-Terminal is a python project that aims to match the website hackertyper, but in a terminal
+
+## Usage
+
+To run hacker-terminal simply ensure there is a file named 'input.txt.hacker' or simply provide the file name like 'python hacker.py testfile.txt'
+When the file is fully printed press 'Esc' to close the program
---
+++ hacker.py
@@ -0,0 +1,41 @@
+import curses, os, sys
+
+if len(sys.argv) is 1 and os.path.isfile('input.txt.hacker'):
+    src_file = open('input.txt.hacker')
+elif len(sys.argv) is 2 and os.path.isfile(sys.argv[1]):
+    src_file = open(sys.argv[1])
+else:
+    print("Either have a file called 'input.txt.hacker' or supply a file "
+    "name as the first argument, e.g. python main.py test.py")
+    exit(1)
+
+myscreen = curses.initscr()
+curses.start_color()
+curses.noecho()
+curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+char = 0
+
+myscreen.getch()
+
+for line in src_file:
+    if char == 27:
+        break
+
+    words = line.split()
+    for word in words:
+        myscreen.addstr(word + ' ', curses.color_pair(1))
+        myscreen.refresh()
+        char = myscreen.getch()
+        if char == 27:
+            break
+
+    myscreen.addstr('\n', curses.color_pair(1))
+    myscreen.refresh()
+
+if char == 27:
+    curses.endpythwin()
+else:
+    while char != 27:
+        char = myscreen.getch()
+
+    curses.endwin()
